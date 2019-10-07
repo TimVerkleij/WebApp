f85465b67a247e397d2483bf3c5b0a79e157ed11
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2019-10-07 09:19:25 +0200

    testing

---
+++ calendar.js
@@ -0,0 +1,114 @@
+/* JavaScript functions voor index.html */
+
+// function hi(){
+//     var name = document.getElementById('yourName').value;
+//     document.getElementsByTagName('h1')[0].innerHTML = "Hi " + name;
+// }
+// function bye(){
+//     var name = document.getElementById('yourName').value;
+//     document.getElementsByTagName('h1')[0].innerHTML = "Bye " + name;
+// }
+// function goodbye(){
+//     var name = document.getElementById('yourName').value;
+//     document.getElementsByTagName('h1')[0].innerHTML = "Goodbye " + name;
+// }
+
+/* JavaScript functions voor Testing.html file */
+
+//maakt variabelen aan om te achterhalen welke dag het is.
+var selectedDate = document.getElementsByTagName('input')[0];
+var selectedDay = document.getElementsByTagName('input')[0];
+
+//maakt variabelen aan voor de dag, maand en jaar.
+var today = new Date();
+var tomorrow = new Date();
+var dd = today.getDate();
+var dd2 = today.getDate()+1;
+var mm = today.getMonth()+1; //January is 0!
+var mm2 = today.getMonth()+1;
+var yyyy = today.getFullYear();
+ //zorgt ervoor dat er bij de dagen 1 tot en met 9 een 0 staat voor het getal. 9 april 2019 wordt
+//  bijvoorbeeld 09-04-2019
+    if(dd<10){
+        dd='0'+dd
+    }
+
+    if(dd2<10){
+        dd2= '0' +dd2;
+    }
+
+    if(mm2<10){
+        mm2== '0' +mm2;
+    }
+
+    if(mm<10){
+        mm='0'+mm
+    }
+
+    if(yyyy % 4 == 0 && mm2 == 2 && dd == 29){
+        dd2 = '0' + 1;
+        mm2 = '0'+3;
+    } else {
+        if(mm2 == 1 || mm2 == 3 || mm2 == 5 ||mm2 == 7 ||mm2 == 8 ||mm2 == 10 ||mm2 == 12){
+            if(dd == 31){
+                    dd2 = '0' + 1;
+                    //console.log(dd, dd2, mm);
+                if(mm>=10){
+                        mm2 = mm2 + 1;
+                    }
+                if(mm<10){
+                        mm2 = mm - '0';
+                        mm2 = mm2 + 1;
+                        //console.log(mm2);
+                    if(mm2<10){
+                        mm2= '0' + mm2;
+                        //console.log(mm2);
+                    }
+                }
+            }
+        }
+        if(mm2 == 4 || mm2 == 6 || mm2 == 9 || mm2 == 11){
+            if(dd == 30){
+                dd2 = '0' +1;
+                //console.log(dd, dd2, mm);
+                if(mm<10){
+                    mm2 = mm - '0';
+                    mm2 = mm2 + 1;
+                    //console.log(mm2);
+                    if(mm2<10){
+                        mm2= '0' + mm2;
+                        //console.log(mm2);
+                    }
+                }
+            }
+        }
+        if(mm2 == 2){
+            if(dd == 28){
+                dd2 = '0' +1;
+                mm2 = '0' +3;
+            }
+        }
+}
+today = yyyy+'-'+mm+'-'+dd;
+tomorrow = yyyy+'-'+mm2+'-'+dd2;
+document.getElementById("date").setAttribute("min", tomorrow); //zorgt ervoor dat je alleen een datum kan kiezen die later is dan vandaag
+document.getElementById("date").setAttribute("value", today); //zet de waarde van de input op vandaag
+
+function datumgekozen(){
+    document.getElementById("time").style.visibility = 'visible';
+    var help = document.getElementById("date").getAttribute("value");
+
+    document.getElementById("14.30").setAttribute("disabled", true);
+    document.getElementById("15.30").setAttribute("disabled", true);
+    document.getElementById("12").setAttribute("disabled", true);
+    document.getElementById("9").setAttribute("disabled", true);
+    document.getElementById("10").setAttribute("disabled", true);
+}
+
+function ready(){
+    var datum = document.getElementById("date").value;
+    var tijd = document.getElementById("time").value;
+    var datumstrng = datum.toString();
+    document.getElementById("p1").style.visibility = 'visible';
+    document.getElementById("p1").innerHTML = "De geselecteerde datum is " + datumstrng + " om " + tijd + " uur.";
+}
