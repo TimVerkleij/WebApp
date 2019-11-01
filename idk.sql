4f62a56f2776742055adf94f63c7571e96b30cbb
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2019-11-01 12:51:35 +0100

    reset ID in database syntax in sql

--- idk.sql
+++ idk.sql
@@ -1,2 +1,4 @@
--- INSERT INTO boekingen (Date, Time, Chair)
--- VALUES ('2019-10-10', '12:00', 3);
+-- DBCC CHECKIDENT (boekingen, RESEED, 0)
+
+INSERT INTO boekingen (Date, Time, Chair)
+VALUES ('2019-10-10', '12:00', 3);
