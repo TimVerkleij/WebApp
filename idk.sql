cd432130f873c8c779ae624496a206a0bc84dd2a
Ryan <Ryan@DevDroplets.ga>
2019-11-15 13:28:31 +0100

    :truck: Changed file structure repo
    Re structured the filing within the whole repository

--- idk.sql
+++
@@ -1,8 +0,0 @@
--- DBCC CHECKIDENT (boekingen, RESEED, 0)
-
--- INSERT INTO boekingen (Date, Time, Chair)
--- VALUES ('2019-10-10', '12:00', 3);
-
--- SELECT * from boekingen;
-
--- DELETE boekingen;
