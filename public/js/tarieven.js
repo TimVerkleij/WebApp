1aa27e5405a6c9eade8392bced786f56d0011089
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2019-12-13 13:35:17 +0100

    Minor bug fixes

--- public/js/tarieven.js
+++ public/js/tarieven.js
@@ -5,12 +5,11 @@ var extraInfo = document.getElementById("footer");
 
 function hide() {
     extraInfo.style.bottom = 0;
-    console.log("hello");
     setTimeout(goDown, 5000);
 }
 
 function goDown(){
-    extraInfo.style.bottom = "-50px";
+    extraInfo.style.bottom = "-100px";
 }
 
 
