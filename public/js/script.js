80c3efb0bfaf4a9cb7bcb0acc89858ec1f8a1f39
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2019-12-19 19:25:29 +0100

    Disable certain time options
    When 4 chairs are occupied the time should disable (not working yet)

--- public/js/script.js
+++ public/js/script.js
@@ -21,6 +21,11 @@ function dateChange() {
             var samson2 = document.getElementById('imgC');
             var gert2 = document.getElementById('imgD');
 
+            var boolBert;
+            var boolErnie;
+            var boolSamson;
+            var boolGert;
+
             response.forEach(myFunction);
 
             function myFunction(value, index, array) {
@@ -31,21 +36,28 @@ function dateChange() {
                             console.log("Bert")
                             bert.setAttribute("disabled", true);
                             bert2.src = '/images/stoel_bezet.png';
+                            boolBert = true;
                         }
                         if (value.chair == "Ernie") {
                             console.log("ernie")
                             ernie.setAttribute("disabled", true);
                             ernie2.src = '/images/stoel_bezet.png';
+                            boolErnie = true;
                         }
                         if (value.chair == "Samson") {
                             console.log("samson")
                             samson.setAttribute("disabled", true);
                             samson2.src = '/images/stoel_bezet.png';
+                            boolSamson = true;
                         }
                         if (value.chair == "Gert") {
                             console.log("gert")
                             gert.setAttribute("disabled", true);
                             gert2.src = '/images/stoel_bezet.png';
+                            boolGert = true;
+                        }
+                        if (boolBert && boolErnie && boolSamson && boolGert){
+                            document.getElementById(value.time).setAttribute("disabled", true);
                         }
                     } else {
                         bert.disabled = false;
