4903239d731109eff9bdf08ae1baede3ae70dd3f
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2019-12-19 15:31:10 +0100

    Implementing get request

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,63 @@
-fetch('/api/getdb2/')
-    .then(v => v.json())
-    .then(response => {
-        response = response.response;
+function dateChange() {
+    fetch('/api/getdb2/')
+        .then(v => v.json())
+        .then(response => {
+            response = response.response;
 
-        // Hier manipuleer je je DOM
+            // Hier manipuleer je je DOM
 
-        response.forEach(myFunction);
+            var datum = document.getElementById('date').value;
+            var tijd = document.getElementById('time').value;
+            console.log(datum);
+            console.log(tijd);
 
-        function myFunction(value, index, array){
-            console.log(value.date);
-        }
-    })
+            var bert = document.getElementById('bert');
+            var ernie = document.getElementById('ernie');
+            var samson = document.getElementById('samson');
+            var gert = document.getElementById('gert');
+
+            var bert2 = document.getElementById('imgA');
+            var ernie2 = document.getElementById('imgB');
+            var samson2 = document.getElementById('imgC');
+            var gert2 = document.getElementById('imgD');
+
+            response.forEach(myFunction);
+
+            function myFunction(value, index, array) {
+                console.log(value);
+                if (value.date == datum) {
+                    if (value.time == tijd) {
+                        if (value.chair == "Bert") {
+                            console.log("Bert")
+                            bert.setAttribute("disabled", true);
+                        }
+                        if (value.chair == "Ernie") {
+                            console.log("ernie")
+                            ernie.setAttribute("disabled", true);
+                        }
+                        if (value.chair == "Samson") {
+                            console.log("samson")
+                            samson.setAttribute("disabled", true);
+                        }
+                        if (value.chair == "Gert") {
+                            console.log("gert")
+                            gert.setAttribute("disabled", true);
+                        }
+                    } else {
+                        bert.disabled = false;
+                        ernie.disabled = false;
+                        samson.disabled = false;
+                        gert.disabled = false;
+                    }
+                } else {
+                    bert.disabled = false;
+                    ernie.disabled = false;
+                    samson.disabled = false;
+                    gert.disabled = false;
+                }
+            }
+
+
+        })
+}
+console.log("it works");
