707f3f42b6f9d590234e089d434e84e40cd39d09
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2020-01-21 16:43:53 +0100

    Added appointment button to price page

--- public/js/tarieven.js
+++ public/js/tarieven.js
@@ -8,8 +8,18 @@ function hide() {
     setTimeout(goDown, 5000);
 }
 
-function goDown(){
+function goDown() {
     extraInfo.style.bottom = "-100px";
 }
 
+document.getElementById("reserverenBtn").className = "notFloating";
 
+window.onscroll = function() { scollFunction() };
+
+function scollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("reserverenBtn").className = "floating";
+    } else {
+        document.getElementById("reserverenBtn").className = "notFloating";
+    }
+}
